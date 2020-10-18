1401e1c1ef2617d55058ff66be5affd384749323
psilvahdx <paulosaron@gmail.com>
2020-10-17 22:55:28 -0300

    api envio de alertas por email

--- srv/alerts.js
+++ srv/alerts.js
@@ -1,9 +1,13 @@
 const cds = require('@sap/cds');
 const dateFormat = require('dateformat');
+const SapCfAxios = require('sap-cf-axios').default;
+const destination = SapCfAxios('ODATA_COLABORADORES');
+const axios = require('axios');
+const qs = require('qs');
 const SequenceHelper = require("./lib/SequenceHelper");
 module.exports = cds.service.impl(async (service) => {
     const db = await cds.connect.to("db");
-    const { Temas, Historico } = service.entities;
+    const { Temas, Historico, AlertasUsuario, EventosAlerta, AppSettings } = service.entities;
 
     service.on("atualizaStatusTemas", async (context) => {
 
@@ -34,24 +38,24 @@ module.exports = cds.service.impl(async (service) => {
                     if (diffDays >= 90) {
                         //Alera Status para Sem Atualização
                         const dUltimoRegistro = dateFormat(dToday, "isoDateTime");
-                        console.log("Atualizando Tema ID:", tema.ID );
-                        const affectedRows = await service.update(Temas).set({ status_ID: 2, ultimoRegistro: dUltimoRegistro  }).where({ ID: tema.ID });
+                        console.log("Atualizando Tema ID:", tema.ID);
+                        const affectedRows = await service.update(Temas).set({ status_ID: 2, ultimoRegistro: dUltimoRegistro }).where({ ID: tema.ID });
                         console.log("Tema Atualizado");
                         nTemasAtualizados = nTemasAtualizados + 1;
                         console.log("affectedRows:", affectedRows);
                         //Histórico
                         const oHist = JSON.parse(JSON.stringify(tema));
-                        delete oHist.ID;                        
-                        
+                        delete oHist.ID;
+
                         oHist.idTema = tema.ID;
                         oHist.status_ID = 2;
                         oHist.ultimoRegistro = dToday;
                         oHist.statusAlterado = "Status alterado pelo sistema após 90 dias";
-                        
+
                         console.log("Atualizando Histórico");
                         const aRows = await service.create(Historico).entries(oHist);
                         console.log("Historico Atualizado", aRows);
-                        
+
                     }
                 }
 
@@ -87,6 +91,186 @@ module.exports = cds.service.impl(async (service) => {
         console.debug('Historico ID:', context.data.ID)
 
     });
+
+    //disparaEmailsAlerta
+    service.on("disparaEmailsAlerta", async (context) => {
+        let oLog = {
+            ID: 0,
+            status: "",
+            message: ""
+        };
+        const dToday = new Date();
+        let nTotalEnvios = 0,
+            aCalendarios = await cds.read(AlertasUsuario),
+            aEventos = await cds.read(EventosAlerta).where({ concluido: false, enviaEmail: true });
+
+        for (let i = 0; i < aEventos.length; i++) {
+            const evento = aEventos[i];
+            console.log("Lendo Evento", evento.ID);
+            const dtAlerta = new Date(evento.dtInicio.substring(0, 4) + "/" +
+                evento.dtInicio.substring(5, 7) + "/" +
+                evento.dtInicio.substring(8, 10));
+
+            console.log("Data Execucao", dToday.toLocaleDateString());
+            console.log("Data Evento", dtAlerta.toLocaleDateString());
+
+            //Recupera Calendario a partir do evento
+            var oCalendarioUser = aCalendarios.find(c => c.ID === evento.alertaUsuario_ID);
+
+            if (oCalendarioUser) {
+
+                //valida se data de envio é a data em que o programa esta executando
+                if (dtAlerta.toLocaleDateString() === dToday.toLocaleDateString()) {
+                    console.log("Evento ocorre na data de Hoje", evento.ID);
+                    //Evento esta relcionado com Status do tema?
+                    if (evento.statusTemas && evento.statusTemas.trim() !== "") {
+                        let aStatusTema = evento.statusTemas.split("|");
+                        console.log("Enviar email para temas com os Status", aStatusTema);
+
+                        var aTemasRepresentante = await cds.read(Temas).where({ representante_ID: oCalendarioUser.usuario_ID, status_ID: aStatusTema });
+                        if (aTemasRepresentante && aTemasRepresentante.length > 0) {
+
+                            //Dispara Email do evento para o Usuário
+                            const emailTemasEnviado = await enviaEmailEvento(evento, oCalendarioUser.usuario_ID);
+
+                            if (emailTemasEnviado) {
+                                nTotalEnvios++;
+                                //Atualiza Evento para Concluído
+                                const evntTemas = await cds.update(EventosAlerta).set({ concluido: true }).where({ ID: evento.ID });
+                            }
+
+                        }
+
+
+                    } else {
+
+                        //Dispara Email do evento para o Usuário
+                        const emailEnviado = await enviaEmailEvento(evento, oCalendarioUser.usuario_ID);
+
+                        if (emailEnviado) {
+                            nTotalEnvios++;
+                            //Atualiza Evento para Concluído
+                            const evnt = await cds.update(EventosAlerta).set({ concluido: true }).where({ ID: evento.ID });
+                        }
+
+                    }
+
+                }
+            }
+
+        }
+
+        oLog.message = `Quantidade de emails enviados: ${nTotalEnvios}`;
+        context.reply(oLog);
+
+    });
+
+    async function enviaEmailEvento(oEvento, sMatricula) {
+
+        let oAppSettings = {};
+        const aAppSettings = await cds.read(AppSettings).where({ ID: 2 });
+        if (aAppSettings.length > 0) {
+            oAppSettings = aAppSettings[0];
+        }
+
+
+        var token = await getBarerToken(oAppSettings).then((token) => {
+            return token;
+        });
+        console.log("token recuperado:", token);
+        console.log("conteudo email", oEvento.conteudo);
+        var oEmailContent = oEvento.conteudo.replace("\"", "'");
+
+        let response = await getEmailColaborador(sMatricula).then(colaborador => {
+
+            //colaborador.Email_Funcionario
+            //Evia Email
+            console.log("Colaborador:", colaborador);
+            return axios({
+                method: 'post',
+                url: `${oAppSettings.urlApi}`,
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/json'
+                },
+                data:
+                {
+                    from: "noreplay@portoseguro.com.br",
+                    to: [
+                        "paulosantos.silva@portoseguro.com.br" //colaborador.Email_Funcionario
+                    ],
+                    subject: oEvento.descricao,
+                    htmlMessage: oEmailContent
+
+                }
+
+            }).then(function (response) {
+                console.log("email enviado com sucesso");
+                return true;
+            }).catch(function (error) {
+                console.log("Erro no envio de email:", error.message);
+            });
+
+        });
+
+        return response;
+    };
+
+    async function getEmailColaborador(sMatricula) {
+
+
+        let sPath = `/xsodata/workflows.xsodata/EmpregadosSet('${sMatricula}')`,
+            oColaborador = {};
+
+        try {
+            const response = await destination({
+                method: "get",
+                url: sPath,
+                headers: {
+                    "content-type": "application/json"
+                },
+                params: {
+                    "$select": "Login_Funcionario,Email_Funcionario"
+                }
+            });
+
+            console.log("ODATA_COLABORADORES_DESTINATION_RESPONSE", response.data);
+
+            oColaborador = response.data.d;
+
+        } catch (error) {
+
+            console.log("ERRO ODATA_COLABORADORES_DESTINATION", error.message);
+        }
+
+        return oColaborador;
+
+    };
+
+    async function getBarerToken(oAppSettings) {
+        try {
+
+            var ret_token = await axios({
+                method: 'post',
+                url: oAppSettings.urlToken,
+                headers: {
+                    'Content-Type': "application/x-www-form-urlencoded"
+                },
+                data: qs.stringify({
+                    client_id: oAppSettings.clientID,
+                    client_secret: oAppSettings.clientSecret,
+                    grant_type: "client_credentials"
+                })
+            }).then(function (response) {
+                return response.data.access_token;
+            }).catch(function (error) {
+                console.log("Erro ao Buscar Token");
+            });
+        } catch (e) {
+            console.log("Erro ao Buscar Token")
+        }
+        return ret_token;
+    };
 
 
 });
